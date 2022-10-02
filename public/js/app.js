668bf44c6110f62fe84ab8f882a9b5278a60c743
Matt Robenolt <matt@ydekproductions.com>
2022-10-01 19:14:25 -0700

    render a graph with d3

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,8 @@ import {
   render,
 } from "https://unpkg.com/preact@10.11.0/dist/preact.module.js";
 
+const { d3 } = window;
+
 const sensors = {
   indoor: 1,
   outdoor: 2,
@@ -26,6 +28,7 @@ class Sensor extends Component {
     this.state = {
       loading: true,
       errored: false,
+      rendered: false,
     };
   }
 
@@ -39,7 +42,7 @@ class Sensor extends Component {
   async refresh() {
     const sensorId = sensors[this.props.id];
     try {
-      const resp = await fetch(`/api/series?id=${sensorId}&limit=1`);
+      const resp = await fetch(`/api/series?id=${sensorId}&limit=720`);
       const data = await resp.json();
       this.setState({
         loading: false,
@@ -54,6 +57,80 @@ class Sensor extends Component {
     }
   }
 
+  setChartRef(dom) {
+    if (this.state.rendered) {
+      return;
+    }
+
+    const margin = { top: 10, right: 30, bottom: 30, left: 60 },
+      width = 460 - margin.left - margin.right,
+      height = 400 - margin.top - margin.bottom;
+
+    const svg = d3
+      .select(dom)
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    const data = this.state.data.series.map((d) => {
+      return {
+        date: new Date(parseInt(d.data_datetime, 10) * 1000),
+        value: d.temp_f - 8,
+      };
+    });
+
+    const x = d3
+      .scaleTime()
+      .domain(
+        d3.extent(data, function (d) {
+          return d.date;
+        })
+      )
+      .range([0, width]);
+
+    svg
+      .append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom(x));
+
+    // Add Y axis
+    const y = d3
+      .scaleLinear()
+      .domain([
+        0,
+        d3.max(data, function (d) {
+          return +d.value;
+        }),
+      ])
+      .range([height, 0]);
+    svg.append("g").call(d3.axisLeft(y));
+
+    // Add the line
+    svg
+      .append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr(
+        "d",
+        d3
+          .line()
+          .x(function (d) {
+            return x(d.date);
+          })
+          .y(function (d) {
+            return y(d.value);
+          })
+      );
+
+    this.setState({
+      rendered: true,
+    });
+  }
+
   render() {
     if (this.state.loading) {
       return h("h1", {}, "loading...");
@@ -65,6 +142,7 @@ class Sensor extends Component {
     }
 
     const latest = this.state.data.series[0];
+    const date = new Date(parseInt(latest.data_datetime, 10) * 1000);
 
     return h(
       "div",
@@ -72,6 +150,11 @@ class Sensor extends Component {
       h("h2", {}, this.props.id),
       h("p", {}, `Temp: ${latest.temp_f - 8}F`),
       h("p", {}, `AQI: ${latest.pm2_5_aqi}`),
+      h("p", {}, `Date: ${date.toLocaleString()}`),
+      h("div", {
+        ref: this.setChartRef.bind(this),
+        style: "width: 300px;",
+      }),
       h(
         "details",
         {},
